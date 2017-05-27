d04a6fcb6936be597adac1d45a3fe07992602ad2
William <willi4m@zju.edu.cn>
2017-05-27 20:09:55 +0800

    ...

---
+++ model.py
@@ -0,0 +1,18 @@
+import numpy as np
+import os
+import tensorflow as tf
+
+
+class Model(object):
+  """
+  
+  """
+  def __init__(self):
+    """"""
+    pass
+
+  # region : Properties
+  # endregion : Properties
+
+
+"""For some reason, do not delete this line."""
---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from ._console import Console
+
+
+console = Console()
