9806afeb55af5338d471faaa61ef6d2c8f2ab986
William <willi4m@zju.edu.cn>
2017-07-06 16:40:52 +0800

    sync ...

---
+++ models/model.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+import numpy as np
+import tensorflow
+
+
+class Model(object):
+  def __init__(self):
+    pass
--- utils/console.py
+++ utils/console.py
@@ -1,45 +1,49 @@
+from __future__ import absolute_import
+
 import pprint
 import time
 
 from sys import stdout
 
 
-_default_length = 80
-_default_title = 'main'
-_prompt = '>>'
-_sub_prompt = "..."
-_pp = pprint.PrettyPrinter()
-_tail_length = 12
-_bar_length =_default_length - _tail_length - 2
+_config = {
+  'default_line_width': 80,
+  'default_title': 'main',
+  'prompt': '>>',
+  'sub_prompt': '...',
+  'tail_width': 12
+}
+_config['bar_width'] = _config['default_line_width'] - _config['tail_width'] - 2
 
 _cache = {}
+_pp = pprint.PrettyPrinter()
 
 
-def start(title=None, length=None):
-  title = title or _default_title
-  length = length or _default_length
-  print("-> Start of %s\n%s" % (title, '-' * length))
+def start(title=None, width=None):
+  title = title or _config['default_title']
+  width = width or _config['default_line_width']
+  print("-> Start of %s\n%s" % (title, '-' * width))
   _cache["title"] = title
 
 
-def end(length=None):
-  title = _cache.pop("title", _default_title)
-  length = length or _default_length
-  print("%s\n|> End of %s" % (('-' * length), title))
+def end(width=None):
+  title = _cache.pop("title", _config['default_title'])
+  width = width or _config['default_line_width']
+  print("%s\n|> End of %s" % (('-' * width), title))
 
 
 def section(contents):
-  print("=" * _default_length)
+  print("=" * _config['default_line_width'])
   print(":: %s" %  contents)
-  print("=" * _default_length)
+  print("=" * _config['default_line_width'])
 
 
-def state(content):
-  print("%s %s" % (_prompt, content))
+def show_status(content):
+  print("%s %s" % (_config['prompt'], content))
 
 
 def supplement(content):
-  print("{} {}".format(_sub_prompt, content))
+  print("{} {}".format(_config['sub_prompt'], content))
 
 
 def pprint(content):
@@ -47,6 +51,14 @@ def pprint(content):
 
 
 def print_progress(index, total, start_time=None):
+  """
+  Print progress bar, the line which cursor is positioned will be overwritten
+  
+  :param index: positive scalar, indicating current work progress
+  :param total: positive scalar, indicating total work 
+  :param start_time: if provided, ETA will be displayed to the right of
+                      the progress bar
+  """
   if start_time is not None:
     duration = time.time() - start_time
     eta = duration / index * (total - index)
@@ -54,8 +66,8 @@ def print_progress(index, total, start_time=None):
   else:
     tail = "{:.0f}%".format(100 * index / total)
 
-  left = int(index * _bar_length / total)
-  right = _bar_length - left
+  left = int(index * _config['bar_width']/ total)
+  right = _config['bar_width'] - left
   mid = '=' if index == total else '>'
   stdout.write('[%s%s%s] %s' %
                ('=' * left, mid, ' ' * right, tail))
@@ -68,6 +80,7 @@ def write_line(content):
 
 
 def clear_line():
-  stdout.write("\r{}\r".format(" " * (_bar_length + _tail_length)))
+  stdout.write("\r{}\r".format(" " * (_config['bar_width'] +
+                                      _config['tail_width'])))
   stdout.flush()
 
