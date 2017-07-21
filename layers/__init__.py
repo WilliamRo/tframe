ccc668f953eadd7e0992438b887259c6e6b10b6a
William <willi4m@zju.edu.cn>
2017-07-21 21:30:04 +0800

    [M] refactored convolutional.py using mro mechanism

--- layers/__init__.py
+++ layers/__init__.py
@@ -3,6 +3,7 @@ from .common import Dropout
 from .common import Flatten
 from .common import Linear
 from .common import Input
+from .common import Reshape
 
 from .convolutional import Conv2D
 from .pooling import MaxPool2D
--- layers/convolutional.py
+++ layers/convolutional.py
@@ -2,26 +2,39 @@ from __future__ import absolute_import
 
 import tensorflow as tf
 
+from ..core import Function
 from .layer import Layer
 from .layer import single_input
 
 from ..utils import get_scale
 
-from tensorflow.python.layers.convolutional import Conv2D as Conv2D_
+from tensorflow.python.layers.convolutional import Conv1D as _Conv1D
+from tensorflow.python.layers.convolutional import Conv2D as _Conv2D
+from tensorflow.python.layers.convolutional import Deconv2D as _Deconv2D
 
 
-class Conv2D(Layer, Conv2D_):
-  """"""
+class _Conv(Layer):
   is_nucleus = True
-
-  full_name = 'convolutional2d'
   abbreviation = 'conv'
 
   @single_input
   def __call__(self, input_=None, **kwargs):
     assert isinstance(input_, tf.Tensor)
-    output = Conv2D_.__call__(self, input_, scope=self.full_name)
+    # TODO: too violent ?
+    output = super(Function, self).__call__(input_, scope=self.full_name)
     self.neuron_scale = get_scale(output)
     return output
 
+
+class Conv1D(_Conv, _Conv1D):
+  full_name = 'convolutional1d'
+
+
+class Conv2D(_Conv, _Conv2D):
+  full_name = 'convolutional2d'
+
+
+class Deconv2D(_Conv, _Deconv2D):
+  full_name = 'deconvolutional2d'
+  abbreviation = 'deconv'
 
