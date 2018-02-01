5a80234f1c4182d13af403a2f5d7438bb7cda774
William <willi4m@zju.edu.cn>
2018-02-01 20:04:09 +0800

    [M] modified polynomial layer

--- layers/parametric_activation.py
+++ layers/parametric_activation.py
@@ -22,7 +22,7 @@ class Polynomial(Layer):
     if order < 0: raise TypeError('!! order must be a non-negative integer')
 
     self.order = order
-    self.coefs = []
+    self.coefs = None
     self._initializer = initializers.get(initializer)
     tail = '{}'.format(order)
     self.full_name += tail
@@ -31,18 +31,18 @@ class Polynomial(Layer):
   @single_input
   def _link(self, input_, **kwargs):
     assert isinstance(input_, tf.Tensor)
-    if len(self.coefs) > 0: tf.get_variable_scope().reuse_variables()
+    if self.coefs is not None: tf.get_variable_scope().reuse_variables()
     # Get input dimension
     D = input_.get_shape().as_list()[1]
     self.neuron_scale = [D]
     # Get variable and calculate output
     order_list = []
+    self.coefs = tf.get_variable(
+      'coefs', shape=(self.order + 1, D), dtype=input_.dtype,
+      initializer=self._initializer)
     x = tf.ones_like(input_)
     for order in range(self.order + 1):
-      coefs = tf.get_variable('coefs_{}'.format(order), shape=[1, D],
-                              dtype=input_.dtype, initializer=self._initializer)
-      self.coefs.append(coefs)
-      order_list.append(tf.multiply(coefs, x))
+      order_list.append(tf.multiply(self.coefs[order], x))
       if order != self.order:
         x = tf.multiply(x, input_, name='x_{}'.format(order + 1))
 
