dfd34469c9b41592a08c7793919952a1e507c70e
William <willi4m@zju.edu.cn>
2019-03-30 15:22:27 +0800

    [M] added saturation penalty mechanism to cell_base

--- nets/rnn_cells/cell_base.py
+++ nets/rnn_cells/cell_base.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 
 from tframe import activations
 from tframe import checker
+from tframe import context
 from tframe import initializers
 from tframe.nets import RNet
 
@@ -51,12 +52,28 @@ class CellBase(RNet):
   def structure_string(self, detail=True, scale=True):
     return self.net_name + self._scale_tail if scale else ''
 
-  def _update_gate(self, x, s, num_units, unit_size, reverse=True,
-                   bias_initializer='zeros'):
-    net_z = self.neurons(x, s, scope='net_z', bias_initializer=bias_initializer)
+  def _update_gate(self, num_units, unit_size, x=None, s=None, net_z=None,
+                   reverse=True, bias_initializer='zeros',
+                   saturation_penalty=0.):
+    if net_z is None:
+      assert x is not None
+      net_z = self.neurons(
+        x, s, scope='net_z', bias_initializer=bias_initializer)
     z = tf.reshape(tf.nn.softmax(tf.reshape(net_z, [-1, unit_size])),
                    [-1, num_units*unit_size], name='z')
+
+    # Calculate opposite
     z_opposite = tf.subtract(1., z)
     if reverse: z, z_opposite = z_opposite, z
+
+    # Add saturation loss to context if necessary
+    if saturation_penalty > 0:
+      context.add_loss_tensor(
+        saturation_penalty * tf.reduce_mean(tf.abs(z - 0.5)))
+    elif saturation_penalty < 0:
+      context.add_loss_tensor(
+        -saturation_penalty * tf.reduce_mean(tf.minimum(z, z_opposite)))
+
     return z, z_opposite
+
 
