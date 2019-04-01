5ca6aa9652b9e51f8a7c178fd9b752782a24e8a9
William <willi4m@zju.edu.cn>
2019-04-01 14:21:48 +0800

    [F] fixed issues that get_optimizer did not work properly

--- configs/config_base.py
+++ configs/config_base.py
@@ -15,8 +15,6 @@ from .monitor_configs import MonitorConfigs
 from .cloud_configs import CloudConfigs
 from .dataset_configs import DataConfigs
 
-from tframe.optimizers.clip_opt import GradientClipOptimizer
-
 
 class Config(
   ModelConfigs,
@@ -195,8 +193,8 @@ class Config(
     return flag
 
   def get_optimizer(self):
-    assert isinstance(self.optimizer, tf.train.Optimizer)
-    assert self.learning_rate is not None
+    from tframe.optimizers.clip_opt import GradientClipOptimizer
+    assert self.optimizer is not None and self.learning_rate is not None
     optimizer = self.optimizer(self.learning_rate)
     if self.clip_threshold > 0:
       optimizer = GradientClipOptimizer(optimizer, self.clip_threshold)
