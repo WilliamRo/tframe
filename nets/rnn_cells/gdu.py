b4aad064724288c571621d9f29eff4f80823a12b
William <willi4m@zju.edu.cn>
2019-04-19 15:59:37 +0800

    [A] added an empty module for GDU

---
+++ nets/rnn_cells/gdu.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+from tframe import checker
+from tframe import linker
+from tframe.nets.rnn_cells.cell_base import CellBase
+
+
+class GDU(CellBase):
+  """Grouped Distributor Unit
+     Reference: gdu2019"""
+  pass
