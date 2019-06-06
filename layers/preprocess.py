3f5c3788084342ddcc6e27756c93a1faa7692850
William <willi4m@zju.edu.cn>
2019-06-06 09:22:50 +0800

    [M] modified Normalize layer

--- layers/preprocess.py
+++ layers/preprocess.py
@@ -14,20 +14,23 @@ class Normalize(Layer):
   full_name = 'normalize'
   abbreviation = 'norm'
 
-  def __init__(self, mu, sigma=None):
-    if not isinstance(mu, np.ndarray):
-      raise TypeError('!! mu should be an ndarray')
+  def __init__(self, mu=None, sigma=None):
+    if mu is not None:
+      assert isinstance(mu, (np.ndarray, float))
+    if sigma is not None:
+      assert isinstance(sigma, (np.ndarray, float))
     self._mu = mu
     self._sigma = sigma
 
 
   @single_input
   def _link(self, x, **kwargs):
-    if isinstance(self._mu, np.ndarray):
+    y = x
+    if self._mu is not None:
       self._mu = tf.constant(self._mu, dtype=hub.dtype)
-    if isinstance(self._sigma, np.ndarray):
+      y = tf.subtract(x, self._mu)
+    if self._sigma is not None:
       self._sigma = tf.constant(self._sigma, dtype=hub.dtype)
-    y = tf.subtract(x, self._mu)
-    if self._sigma is not None: y = tf.divide(y, self._sigma)
+      y = tf.divide(y, self._sigma)
     return y
 
