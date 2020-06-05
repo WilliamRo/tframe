6a3bfb65b7f7b14500bcc448688d4abdaa51eeff
rscv5 <renshuchan@163.com>
2020-06-05 13:55:34 +0800

    [M] modified utils/dispaly/img_utils.py

--- utils/display/img_utils.py
+++ utils/display/img_utils.py
@@ -3,18 +3,24 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
+from matplotlib import colors
+
+
+def get_rgb_by_name(name, dtype=float):
+  rgb = np.array(colors.hex2color(colors.cnames[name]))
+  if dtype is not float: rgb = (rgb * 255).astype(np.int)
+  return rgb
 
 
 def int2listedcmap(indices, cmap):
   assert isinstance(indices, np.ndarray) and isinstance(cmap, list)
-  from matplotlib import colors
-  cmap = np.array([colors.hex2color(colors.cnames[name]) for name in cmap])
+  cmap = np.array([get_rgb_by_name(name) for name in cmap])
   return cmap[indices]
 
 
 if __name__ == '__main__':
   from matplotlib import colors
-  print(colors.hex2color(colors.cnames['orange']))
+  print(get_rgb_by_name('white', int))
   cmap = np.array([[-2, -4], [2, 4.0]])
   indices = np.array([[0, 1], [1, 0]])
   im = cmap[indices]
