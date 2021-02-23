377a28ae2bfe5a2e9ebdea046becc68531694040
WilliamRo <walienluo@gmail.com>
2021-02-23 17:15:39 +0800

    [M] updated template.py

--- institute/tflab/template.py
+++ institute/tflab/template.py
@@ -5,12 +5,15 @@ from tframe import console
 
 console.suppress_logging()
 console.start('Template')
-
+tf.InteractiveSession()
+# =============================================================================
 # Put your codes below
+# =============================================================================
 x = np.arange(12).reshape(6, 2)
-tensor_x = tf.placeholder(dtype=tf.float32)
-with tf.Session() as sess:
-  console.eval_show(tensor_x, name='x', feed_dict={tensor_x: x})
 
+tensor_x = tf.placeholder(dtype=tf.float32)
+console.eval_show(tensor_x, name='x', feed_dict={tensor_x: x})
+# =============================================================================
 # End of the script
+# =============================================================================
 console.end()
