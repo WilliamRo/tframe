d830896a284ab0de35f8696fd236658445d42be5
WilliamRo <walienluo@gmail.com>
2021-02-27 16:14:19 +0800

    [M] added a topic

---
+++ institute/topics.py
@@ -0,0 +1,20 @@
+"""
+                 1. A Network Able to Clean Data
+                     (inited on Feb 27 ,2021)
+
+1.1 Background
+Training data with inaccurate annotation.
+Say, a model trained with 10000 samples with 1000 inaccurate annotation
+would be inferior to the same structured model trained with 5000 samples
+with accurate annotation. := <1>
+
+1.2 Experiment Design
+1.2.1 MNIST
+Try to validate <1> by deliberately modified a part of the labels to
+erroneous ones. Try different numbers.
+
+1.3 Method
+1.3.1
+
+
+"""
