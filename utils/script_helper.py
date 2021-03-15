c47a1c6b1d81c3484f481cac86be6956a0283c93
WilliamRo <walienluo@gmail.com>
2021-03-15 19:56:33 +0800

    [M] fixed a bug

--- utils/script_helper.py
+++ utils/script_helper.py
@@ -173,7 +173,6 @@ class Helper(object):
       self.common_parameters[flag_name] = val
       self._show_flag_if_necessary(flag_name, val)
 
-  @check_flag_name
   def set_hp_property(self, name, hp_type, scale=None):
     assert name in self.pot.hyper_parameter_keys
     self.pot.set_hp_properties(name, hp_type, scale)
