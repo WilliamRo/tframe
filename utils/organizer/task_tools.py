93e820483f56477d187e8521a7b8175c563f1515
William <luo.wei@zju.edu.cn>
2022-10-11 16:52:06 +0800

    [M] fixed `update_job_dir not working for trained model`

--- utils/organizer/task_tools.py
+++ utils/organizer/task_tools.py
@@ -14,5 +14,15 @@ def update_job_dir(id, model_name):
 
   file, _, _, _ = list(traceback.extract_stack())[-2]
   task_dir = os.path.dirname(file)
-  th.job_dir = os.path.join(task_dir, '{:02d}_{}'.format(id, model_name))
+
+  # TODO: the patch below is used currently
+  job_fn = '{:02d}_{}'.format(id, model_name)
+  if any([f'{job_fn}/checkpoints' in task_dir for c in ('/', '\\')]):
+    # For trained model, the task module is located inside the
+    # corresponding checkpoints folder,
+    th.job_dir = os.path.dirname(os.path.dirname(task_dir))
+  else:
+    # While running normal task module, `task_dir` should be the folder
+    # containing this module file
+    th.job_dir = os.path.join(task_dir, job_fn)
 
