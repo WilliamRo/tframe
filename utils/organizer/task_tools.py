3b7bedc36d42def61c3f3afe03753851377154ec
William <luo.wei@zju.edu.cn>
2022-11-04 17:51:28 +0800

    [M] fixed skopt not working issue +1

--- utils/organizer/task_tools.py
+++ utils/organizer/task_tools.py
@@ -11,22 +11,24 @@ def update_job_dir(id, model_name):
   from tframe import hub as th
   from tframe.utils.local import check_path
 
+  dirname = os.path.dirname
   # Job folder name
   job_fn = '{:02d}_{}'.format(id, model_name)
   file_stack = [check_path(pkg[0]) for pkg in list(traceback.extract_stack())]
+  task_dir = os.path.dirname(file_stack[-2])
 
   # Case (1): running t-file located in checkpoints model
-  if os.path.join(job_fn, 'checkpoints') in file_stack[-2]:
-    th.job_dir = os.path.dirname(os.path.dirname(file_stack[-2]))
+  if os.path.join(job_fn, 'checkpoints') in task_dir:
+    th.job_dir = os.path.dirname(os.path.dirname(task_dir))
     return
 
   # Case (2): running s-file
-  task_dir = os.path.dirname(file_stack[-2])
   if th.job_dir not in task_dir:
     # Find correct job_dir for algorithms such as skopt
     for fn in file_stack[::-1]:
       if th.job_dir in fn:
-        return os.path.dirname(fn)
+        th.job_dir = os.path.dirname(fn)
+        return
     raise AssertionError('!! failed to extract job_dir while running s-file')
 
   # Case (3): running t-file from common location, `task_dir` should
